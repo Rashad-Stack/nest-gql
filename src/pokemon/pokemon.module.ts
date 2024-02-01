7c569cd580c646b1a29ad9cab75899a618d74076
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-01 16:48:05 +0600

    Add PokemonEntity to PokemonModule imports and update PokemonService with CRUD operations

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PokemonController } from "./pokemon.controller";
+import { PokemonEntity } from "./pokemon.entity";
 import { PokemonResolver } from "./pokemon.resolver";
 import { PokemonService } from "./pokemon.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([PokemonEntity])],
   providers: [PokemonService, PokemonResolver],
   controllers: [PokemonController],
 })
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { PokemonEntity } from "./pokemon.entity";
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  constructor(
+    @InjectRepository(PokemonEntity)
+    private readonly pokemonRepository: Repository<PokemonEntity>,
+  ) {}
+
+  async findAll(): Promise<PokemonEntity[]> {
+    return await this.pokemonRepository.find();
+  }
+
+  async findOneById(id: string): Promise<PokemonEntity> {
+    const pokemon = await this.pokemonRepository.findOne({
+      where: { id },
+    });
+
+    if (!pokemon) throw new NotFoundException();
+
+    return pokemon;
+  }
+
+  async create(pokemon: PokemonEntity): Promise<PokemonEntity> {
+    return await this.pokemonRepository.save(pokemon);
+  }
+
+  async update(id: string, pokemon: PokemonEntity): Promise<PokemonEntity> {
+    const oldPokemon = await this.findOneById(id);
+    await this.pokemonRepository.update(oldPokemon.id, pokemon);
+    return await this.findOneById(oldPokemon.id);
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const pokemon = await this.findOneById(id);
+    await this.pokemonRepository.delete(pokemon.id);
+    return true;
+  }
+}
